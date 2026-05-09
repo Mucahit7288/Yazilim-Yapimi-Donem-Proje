992f25a32a92935a99555d3f16ad44f1b4d229e9
Ahmet Arda <a.arda20056@gmail.com>
2026-05-09 19:06:07 +0300

    SqlLite veri tabanı kuruldu

---
+++ lib/core/database/database_helper.dart
@@ -0,0 +1,41 @@
+import 'package:path/path.dart';
+import 'package:sqflite/sqflite.dart';
+
+class DatabaseHelper {
+  static final DatabaseHelper instance = DatabaseHelper._init();
+  static Database? _database;
+
+  DatabaseHelper._init();
+
+  Future<Database> get database async {
+    if (_database != null) return _database!;
+    _database = await _initDB('kelime_oyunu.db');
+    return _database!;
+  }
+
+  Future<Database> _initDB(String fileName) async {
+    final dbPath = await getDatabasesPath();
+    final path = join(dbPath, fileName);
+
+    return await openDatabase(
+      path,
+      version: 1,
+      onCreate: _createDB,
+    );
+  }
+
+  Future<void> _createDB(Database db, int version) async {
+    await db.execute('''
+      CREATE TABLE Users (
+        UserID INTEGER PRIMARY KEY AUTOINCREMENT,
+        UserName TEXT NOT NULL,
+        Password TEXT NOT NULL
+      )
+    ''');
+  }
+
+  Future<void> close() async {
+    final db = await instance.database;
+    db.close();
+  }
+}
